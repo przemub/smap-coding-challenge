2691fc068b881ee0740edce3c3e4600c0dc034e5
Przemysław Buczkowski <przemub@przemub.pl>
2018-08-21 10:37:06 +0100

    Implement import command

--- REPORT.md
+++ REPORT.md
@@ -1,8 +1,22 @@
 Full-stack coding challenge
 
-By Przemysław Buczkowski, 20th of August 2018
+By Przemysław Buczkowski, 20th/21st of August 2018
+
+* General comments
+    * Code is sometimes over-commented in places where I wanted to direct your attention to minor implementation design choices.
+
+* Environment
+    * As it turned out, the version of Django specified in requirements.txt is incompatible with Python 3.7 (cf. https://code.djangoproject.com/ticket/28814).
+    Not wanting to mess with versions you specified, I compiled and installed Python 3.6 on my system.
+    Unless it is intentional, I strongly suggest updating requirements.txt :)
 
 * Models
-    * User model named MeterUser to avoid confusion with built-in User model
+    * User model named MeterUser to avoid confusion with the built-in User model
     * Consumption stored as a decimal type to ensure accuracy (up to one mWh)
         * The trade-off is lower performance which will be taken care of later.
+
+* Import
+    * Considering the nature of this project, I opted for a simple implementation utilising
+    built-in csv module and Django ORM. Unfortunately it is slow. If we had to import a lot of data, it would be beneficial
+    to optimise it - I would use pandas library to import CSV files directly into the database and parallelise the task.
+    
--- dashboard/consumption/management/commands/import.py
+++ dashboard/consumption/management/commands/import.py
@@ -1,8 +1,76 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from django.utils import timezone
+
+import csv
+import decimal
+import os
+import sys
+
+from consumption.models import MeterUser, ConsumptionEntry
 
 
 class Command(BaseCommand):
     help = 'import data'
 
     def handle(self, *args, **options):
-        print("Implement me!")
+        # TODO: Add an option to open data in a different directory.
+        # Ensure that the command works even if not called from the project directory
+        script_directory = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+        # Using os.path.join to ensure portability
+        try:
+            users_csv = open(os.path.join(script_directory, "..", "data", "user_data.csv"), "r")
+        except FileNotFoundError:
+            raise CommandError("user_data.csv not found.")
+
+        users_reader = csv.reader(users_csv)
+
+        header = next(users_reader)
+        if header != ["id", "area", "tariff"]:
+            raise CommandError("Invalid user_data.csv header.")
+
+        for row in users_reader:
+            try:
+                user_id = int(row[0])
+            except ValueError:
+                print("Invalid user ID %s. Skipping." % row[0], file=sys.stderr)
+
+            print("Processing user %d." % user_id)
+
+            try:
+                consumption_csv = open(os.path.join(script_directory, "..", "data", "consumption", "%d.csv" % user_id),
+                                       "r")
+            except FileNotFoundError:
+                print("Consumption file for user %d not found. Skipping." % user_id, file=sys.stderr)
+
+            try:
+                with transaction.atomic():
+                    user = MeterUser.objects.get_or_create(id=user_id, area=row[1], tariff=row[2])[0]
+
+                    consumption_reader = csv.reader(consumption_csv)
+                    if next(consumption_reader) != ["datetime", "consumption"]:
+                        raise ValueError
+
+                    # Make sure that datetimes are unique for any given user.
+                    # In case an entry already exists, overwrite it.
+
+                    for reading in consumption_reader:
+                        current_tz = timezone.get_current_timezone()
+                        reading_date = timezone.datetime.strptime(reading[0], "%Y-%m-%d %H:%M:%S")
+                        reading_date = current_tz.localize(reading_date)
+
+                        reading_value = decimal.Decimal(reading[1])
+
+                        try:
+                            consumption = ConsumptionEntry.objects.get(user=user, datetime=reading_date)
+                            consumption.consumption = reading_value
+                            consumption.save()
+                        except ConsumptionEntry.DoesNotExist:
+                            consumption = ConsumptionEntry(user=user, datetime=reading_date, consumption=reading_value)
+                            consumption.save()
+
+            except ValueError:
+                print("CSV file for user %d is malformed. Roll-backed changes." % user_id, file=sys.stderr)
+
+        print("Finished!")
