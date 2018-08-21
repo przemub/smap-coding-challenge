5a43a3a59e2813b282ca6bb1ed0cf7176f44e4a6
Przemysław Buczkowski <przemub@przemub.pl>
2018-08-21 22:16:51 +0100

    The first test

--- dashboard/consumption/tests.py
+++ dashboard/consumption/tests.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+from consumption.models import MeterUser, ConsumptionEntry
+
+
+class ModelTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_create_user(self):
+        MeterUser.objects.create(id=500, area="Gdynia", tariff="udręka13")
+        with self.assertRaises(IntegrityError):
+            MeterUser.objects.create(id=500, area="Gdańsk", tariff="udręka14")
+
+
